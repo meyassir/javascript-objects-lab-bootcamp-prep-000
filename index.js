5b7b7a2495204d06fd43374e3e8a772dbf2fa86c
yasser mimouni <yasmimouni2@gmail.com>
2017-11-05 14:58:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function updateObjectWithKeyAndValue(object, key, value)
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 {
-  return object.assign({}, object, {[key]: value});
+  object = Object.assign({}, object, {[key]: value});
 }
